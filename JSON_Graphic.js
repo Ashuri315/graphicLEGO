2d3545c5626f00c6d97098f9159ec77a18283ca9
Ashuri315 <160205199+Ashuri315@users.noreply.github.com>
2024-08-21 21:58:51 -0600

    Update and rename JSON_Graphic to JSON_Graphic.js
    Creation of the Json file

---
+++ JSON_Graphic.js
@@ -0,0 +1,7 @@
+[
+    {"Category":"Hard to Find","Amount":20},
+    {"Category":"Exclusive","Amount":10},
+    {"Category":"BestSeller","Amount":5},
+    {"Category":"Last Chance","Amount":17},
+    {"Category":"New","Amount":7}
+]
